5ed431b5691bef451fd0134605b3581afcd8a7aa
rodolfo <rodolfovasconcellos88@gmail.com>
2024-03-09 15:13:02 -0300

    desafio finalizado

--- script.js
+++ script.js
@@ -50,6 +50,39 @@ function verificaFibonacci(numero) {
 const numero = 21;
 console.log(verificaFibonacci(numero)); // Saída: 21 pertence à sequência de Fibonacci.
 
-//3 - Descubra a lógica e complete o próximo elemento:
-//O pŕoximo número da sequencia é 200
+/* 3 - Descubra a lógica e complete o próximo elemento:
+RESPOSTA: O pŕoximo número da sequencia é 200 */
 
+/* 4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada
+ em uma sala diferente. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar
+ os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, 
+qual interruptor controla cada lâmpada?
+
+RESPOSTA: Ligue um dos interruptores e espere um pouco. Desligue e ligue um segundo interruptor.
+ Vá até a sala. A lâmpada desligada e quente corresponde ao primeiro interruptor, 
+ a lâmpada acesa ao segundo e a lâmpada apagada e fria ao terceiro.
+*/
+
+/* 5) Escreva um programa que inverta os caracteres de um string.
+*/
+
+function inverterString(str) {
+    
+    let caracteres = str.split('');
+    let caracteresInvertidos = [];
+
+    for (let i = caracteres.length - 1; i >= 0; i--) {
+        
+        caracteresInvertidos.push(caracteres[i]);
+    }
+
+    let stringInvertida = caracteresInvertidos.join('');
+
+    
+    return stringInvertida;
+}
+
+let minhaString = "Olá, mundo!";
+let stringInvertida = inverterString(minhaString);
+console.log(stringInvertida);
